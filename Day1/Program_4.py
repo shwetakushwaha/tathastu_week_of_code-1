f0733216385783b24b998ececeb03f7bcb34ce4e
abhijeet4349 <65857732+abhijeet4349@users.noreply.github.com>
2020-05-24 19:25:59 +0530

    Create Program_4.py

---
+++ Day1/Program_4.py
@@ -0,0 +1,7 @@
+sp=int(input("enter the selling price of item"))
+cp=int(input("enter the cost price of item"))
+profit=sp-cp
+profit=sp-cp
+print("the profit on commodity is:",profit)
+newsp=1.05*profit+cp
+print("after increasing profit by 15% the new selling price is:",newsp)
